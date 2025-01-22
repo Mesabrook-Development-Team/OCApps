f6340fa55164db120e5cacbdd9a3b78c2ae442a5
Adam Clussman <cnwaj@hotmail.com>
2025-01-22 16:46:04 -0600

    Start of Shipping & Receiving software

---
+++ sar/receiving.lua
@@ -0,0 +1,86 @@
+local term = require('term')
+local keyboard = require('keyboard')
+
+local module = {}
+
+function nl()
+    local col, row = term.getCursor()
+    term.setCursor(1, row + 1)
+end
+
+function performScan()
+
+end
+
+function acceptBOL()
+    local currentOption = 1
+    term.setCursorBlink(false)
+
+    while true do
+        term.clear()
+        term.write("By scanning a Bill of Lading, you are contractually accepting this railcar as received as described.", true)
+        nl()
+        nl()
+        term.write("Select an option below and press enter")
+        nl()
+        term.write("[")
+        if currentOption == 1 then
+            term.write("x")
+        else
+            term.write(" ")
+        end
+        term.write(") - Scan page from scanner")
+        nl()
+        
+        term.write("[")
+        if currentOption == 1 then
+            term.write("x")
+        else
+            term.write(" ")
+        end
+        term.write(") - Finish scanning")
+
+        local _,_,_,keyCode = term.pull('key_down')
+        if keyCode == keyboard.keys.up and currentOption > 1 then
+            currentOption = currentOption - 1
+        else if keyCode == keyboard.keys.down and currentOption < 2 then
+            currentOption = currentOption = 1
+        else if keyCode == keyboard.keys.enter then
+            if currentOption == 1 then -- Do scan
+                -- scan
+            else if currentOption == 2 then -- exit
+                return
+            end
+        end
+    end
+end
+
+module.menu = function()
+    while true do
+        term.clear()
+
+        term.write('* RECEIVING MENU *')
+        nl()
+        nl()
+        term.write('1 - Accept Bills Of Lading')
+        nl()
+        term.write('2 - Process Cars on Track')
+        nl()
+        term.write('3 - Return to Main Menu')
+        nl()
+        nl()
+        term.write('Enter an option:')
+        local opt = term.read()
+        local optNum = tonumber(opt)
+
+        if optNum == 1 then -- Accept BOL
+            acceptBOL()
+        else if optNum == 2 then -- Cars on track
+            -- Process cars on track
+        else if optNum == 3 then -- Exit
+            return
+        end
+    end
+end
+
+return module
---
+++ sar/sar.lua
@@ -0,0 +1,191 @@
+local fs = require("filesystem")
+local term = require("term")
+local serialization = require("serialization")
+local receiving = require("receiving")
+local mesaApi = require('mesasuite_api')
+local json = require('json')
+
+function nl()
+    local col, row = term.getCursor()
+    term.setCursor(1, row + 1)
+end
+
+function mainMenu()
+    while true do
+        term.clear();
+
+        term.write('* SHIPPING & RECEIVING *')
+        nl()
+        nl()
+        term.write('1 - Perform Shipping')
+        nl()
+        term.write('2 - Perform Receiving')
+        nl()
+        term.write('3 - Exit')
+        nl()
+        nl()
+        term.write('Enter an option:')
+        local opt = term.read()
+        local optNum = tonumber(opt)
+        if optNum == 1 then -- Shipping
+            -- Call shipping
+        else if optNum == 2 then -- Receiving
+            receiving.menu()
+        else if optNum == 3 then -- Exit
+            return
+        end
+    end
+end
+
+function configureLocation()
+    term.clear()
+    term.write("Loading your companies...")
+    nl()
+
+    local success, jsonStr = mesaApi.request('company', 'Company/GetForEmployee')
+    if success == false then
+        term.write('Unable to fetch your current companies')
+        nl()
+        return false
+    end
+
+    local companyArray = json.parse(jsonStr)
+    for companyIndex,company in ipairs(companyArray) do
+        for locationIndex,location in ipairs(company.Locations) do
+            success, jsonStr = mesaApi.request('company', 'GetForCurrentUser/' .. location.LocationID)
+            if success == false then
+                table.remove(companyArray, companyIndex)
+                break
+            end
+
+            local locationEmployee = json.parse(jsonStr)
+            if locationEmployee.ManagePurchaseOrders == false then
+                table.remove(company.Locations, locationIndex)
+            end
+        end
+
+        if #company.Locations == 0 then
+            table.remove(companyArray, companyIndex)
+        end
+    end
+
+    if #companyArray == 0 then
+        term.clear()
+        term.write('You do not have permission to manage Purchase Orders in any Company', true)
+        nl()
+        return false
+    end
+
+    local selectedCompanyIndex = 0
+    while true do
+        term.clear()
+        term.write('Your companies:')
+        nl()
+        nl()
+        for i,company in ipairs(companyArray) do
+            term.write(i .. ' - ' .. company.Name)
+            nl()
+        end
+        nl()
+        term.write('Select a company:')
+        local opt = term.read()
+        local optNum = tonumber(opt)
+
+        if optNum > 0 and optNum <= #companyArray then
+            selectedCompanyIndex = optNum
+            break
+        end
+    end
+
+    local selectedCompany = companyArray[selectedCompanyIndex]
+    local selectedLocationIndex = 0
+    while true do
+        term.clear()
+        term.write('Your locations:')
+        nl()
+        nl()
+        for i,location in ipairs(company.Locations) do
+            term.write(i .. ' - ' .. location.Name)
+            nl()
+        end
+        nl()
+        term.write('Select a location:')
+        local opt = term.read()
+        local optNum = tonumber(opt)
+
+        if optNum > 0 and optNum <= #company.Locations then
+            selectedLocationIndex = optNum
+            break
+        end
+    end
+
+    local selectedLocation = company.Locations[selectedLocationIndex]
+    local fileContents = {CompanyID=selectedCompany.CompanyID, LocationID=selectedLocation.LocationID}
+
+    local file = io.open('/etc/sar/loc.cfg', 'w')
+    file:write(serialization.serialize(fileContents))
+    file:close()
+end
+
+function verifySetup()
+    term.clear()
+    term.write("Application is loading...")
+
+    local isLoggedIn = mesaApi.request('company', 'Company/GetAll')
+    if isLoggedIn == false then
+        term.clear()
+        term.write("You're not logged into MesaSuite")
+        nl()
+        term.write("Run mesalogin and try again")
+        nl()
+        return false
+    end
+
+    fs.makeDirectory('/etc/sar')
+
+    if not fs.exists('/etc/sar/loc.cfg') then
+        while true do
+            term.clear()
+            term.write('Required Company and Location information is missing.')
+            nl()
+            nl()
+            term.write('Configure now (y/n)?')
+            local opt = term.read()
+            if string.lower(opt) == "y" then -- Do configuration
+                if configureLocation() == false then
+                    return false
+                end
+                break
+            else
+                nl()
+                return false
+            end
+        end
+    end
+
+    local configFile = io.open('/etc/sar/loc.cfg', 'r')
+    local fileContentsStr = configFile:read("*a")
+    local fileContents = serialization.unserialize(fileContentsStr)
+
+    if tonumber(fileContents.CompanyID) == nil or tonumber(fileContents.LocationID) == nil then
+        while true do
+            term.clear()
+            term.write('Required Company and Location information is corrupted.')
+            nl()
+            nl()
+            term.write('Reconfigure now (y/n)?')
+            local opt = term.read()
+            if string.lower(opt) == "y" then -- Do configuration
+                if configureLocation() == false then
+                    return false
+                end
+                break
+            else
+                nl()
+                return false
+            end
+        end
+    end
+
+    return true
+end
