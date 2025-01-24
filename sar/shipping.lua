0f9939e1c44231832b29743f629a305c2a5a6e65
Adam Clussman <cnwaj@hotmail.com>
2025-01-24 12:27:12 -0600

    SAR: Add manual entry

--- sar/shipping.lua
+++ sar/shipping.lua
@@ -2,26 +2,37 @@ local term = require('term')
 local serialization = require('serialization')
 local mesaApi = require('mesasuite_api')
 local json = require('json')
+local text = require('text')
 
+local companyID = nil
+local locationID = nil
 local module = {}
 
 local function nl()
-    local col, row = term.getCursor()
-    term.setCursor(1, row + 1)
+    local _, row = term.getCursor()
+    local width,height = term.getViewport()
+    if row == height then
+       local gpu = term.gpu()
+       -- Shift all lines up by one
+        gpu.copy(1, 2, width, height - 1, 0, -1)
+
+        -- Clear the last line
+        gpu.fill(1, height, width, 1, " ")
+        term.setCursor(1, height)
+    else
+        term.setCursor(1, row + 1)
+    end
 end
 
 local selectedCars = {} -- Key: Reporting Mark, Value: Railcar ID
 
 -- Process from MesaSuite
 local function processFromMesaSuite()
+    selectedCars = {}
     term.write('Selecting from MesaSuite...')
 
-    local locationFile = io.open('/etc/sar/loc.cfg', 'r')
-    local fileContents = serialization.unserialize(locationFile:read('*a'))
-    locationFile:close()
-
     local getFromMesa = function(resource)
-        local success, jsonStr = mesaApi.request('company', resource, {CompanyID=fileContents.CompanyID, LocationID=fileContents.LocationID})
+        local success, jsonStr = mesaApi.request('company', resource, nil, {CompanyID=companyID, LocationID=locationID})
         if success == false then
             return nil
         else
@@ -71,8 +82,54 @@ local function processFromMesaSuite()
     end
 end
 
+-- Process manual entry
+local function processManualEntry()
+    while true do
+        term.clear()
+        term.write('Cars to Ship:')
+        nl()
+        term.write('---------------')
+        nl()
+        if next(selectedCars) == nil then
+            term.write('* No cars selected *')
+            nl()
+        else
+            for reportingMark,_ in pairs(selectedCars) do
+                term.write(reportingMark)
+                nl()
+            end
+        end
+        term.write('---------------')
+        nl()
+        term.write('Enter a Reporting Mark, or blank to exit:')
+        local reportingMark = text.trim(term.read())
+
+        if reportingMark == '' then
+            return
+        else
+            local success, jsonStr = mesaApi.request('company', 'Railcar/GetByReportingMark/' .. reportingMark, nil, {CompanyID=companyID, LocationID=locationID})
+            if success == false then
+                term.write('Could not find railcar with that reporting mark')
+                nl()
+                term.write('Press enter to continue')
+                term.read()
+            else
+                local railcar = json.parse(jsonStr)
+                selectedCars[railcar.ReportingMark .. railcar.ReportingNumber] = railcar.RailcarID
+            end
+        end
+    end
+end
+
 -- Menu
 module.menu = function()
+    local locationFile = io.open('/etc/sar/loc.cfg', 'r')
+    local fileContents = serialization.unserialize(locationFile:read('*a'))
+    locationFile:close()
+
+    companyID = fileContents.CompanyID
+    locationID = fileContents.LocationID
+
     while true do
         term.clear()
 
@@ -80,7 +137,7 @@ module.menu = function()
         nl()
         term.write('---------------')
         nl()
-        if #selectedCars == 0 then
+        if next(selectedCars) == nil then
             term.write('* No cars selected *')
             nl()
         else
@@ -97,7 +154,9 @@ module.menu = function()
         nl()
         term.write('3 - Manual Entry')
         nl()
-        term.write('4 - Exit')
+        term.write('4 - Perform Loading')
+        nl()
+        term.write('5 - Exit')
         nl()
         term.write('Enter an option:')
         local opt = term.read()
@@ -108,11 +167,15 @@ module.menu = function()
         elseif optNum == 2 then -- Select from MesaSuite
             processFromMesaSuite()
         elseif optNum == 3 then -- Manual Entry
-            -- Process manual entry
-        elseif optNum == 4 then -- Exit
+            processManualEntry()
+        elseif optNum == 4 then -- Perform Loading
+            -- Perform loading
+        elseif optNum == 5 then -- Exit
             break
         end
     end
+
+    selectedCars = {}
 end
 
 return module
